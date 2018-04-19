4faab563dcdead2fca29c5c9f0c373e013494d9a
Iswanul Umam <umam@sepulsa.com>
2018-04-19 19:20:38 +0700

    finish create sequelize

--- controllers/index.js
+++ controllers/index.js
@@ -6,12 +6,23 @@ let Author = models.Author;
 
 class AutorController {
   static auhorAdd(values) {
+
     let firstName = values[0];
     let lastName = values[1];
     let religion = values[2];
     let gender = values[3];
     let age = values[4];
     
+    Author.create({ firstName, lastName, religion, gender, age, createdAt: new Date(), updatedAt: new Date() })
+      .then(author => {
+        if (author) {
+          views.show(`Data baru berhasil dimasukan!`);
+          process.exit(0);
+        } else {
+          views.show(`Gagal memasukan data!`);
+          process.exit(0);
+        }
+      });
   }
   static authorReadOne(values) {
     let id = Number(values[0]);
